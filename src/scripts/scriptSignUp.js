6e8d594ccf3ee578eee0ee42de0beac1c1e9568f
chavaxx <sgaytani@live.com>
2021-04-28 21:47:37 -0500

    signUp casi completo, subir para pruebas

---
+++ src/scripts/scriptSignUp.js
@@ -0,0 +1,47 @@
+const url = "http://35.223.20.167:8103/login/newUsuario"
+
+
+function signUp(){
+
+ const myNombre = $('#nombreUsuario').val();
+ const myFechaNacimientoUsuario = $('#fechaNacimientoUsuario').val();
+ const myNumeroTelefonicoUsuario = $('#numeroTelefonicoUsuario').val();
+ const myColonia = $('#coloniaUsuario').val();
+ const myEmail = $('#emailUsuario').val();
+ const myApellidos = $('#apellidoUsuario').val();
+ const myOcupacion = $('#ocupacionUsuario').val();
+ const myCodigoPostal = $('#codigoPostalUsuario').val();
+ const myCalle = $('#calleUsuario').val();
+ const myPassword = $('#passwordUsuario').val();
+
+ const myUsuario={
+    nombre: myNombre,
+    apellido: myApellidos,
+    correo: myEmail,
+    contraseña: myPassword,
+    codigoPostal: myCodigoPostal,
+    colonia: myColonia,
+    calle: myCalle,
+    fechaNacimiento: myFechaNacimientoUsuario,
+    ocupacion: myOcupacion,
+    numeroTelefonico: myNumeroTelefonicoUsuario
+  }
+  console.log(myUsuario)
+  
+  $.ajax({
+    url: url,
+    type: 'post',
+    dataType: 'json',
+    contentType: 'application/json',
+    data: JSON.stringify(myUsuario),
+    success: function (){
+        document.location.href = "home.html";
+        
+    },
+    error: function(e){
+        alert(e);
+        
+    }
+    
+})
+}
